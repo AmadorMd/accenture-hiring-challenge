a35b6b762e3abc0dadd27df809bdfd8a6a65aeb5
Luis Amador <amadormd912@gmail.com>
2023-02-11 18:20:48 -0600

    add: create component Post

--- src/App.js
+++ src/App.js
@@ -3,21 +3,43 @@ import { useEffect, useState } from 'react'
 const ALL_POST_ENDPOINT = 'https://jsonplaceholder.typicode.com/posts'
 function App () {
   const [posts, setPosts] = useState([])
+  const [urlImage, setUrlImage] = useState()
   // get all the post to display in site
   useEffect(() => {
     fetch(ALL_POST_ENDPOINT)
       .then(res => res.json())
       .then(data => setPosts(data))
   }, [])
-  return (
-    <main>
-      <div className='container max-w-screen-md mx-auto'>
-        <ul>
-          {posts.map(post => (
-            <li key={post.id}>{post.title}</li>
-          ))}
-        </ul>
+  // get a radom Image
+  useEffect(() => {
+    fetch('https://source.unsplash.com/random?orientation=landscape')
+      .then(res => setUrlImage(res.url))
+  }, [])
+  // post component
+  const Post = ({ title, body }) => {
+    return (
+      <div className='bg-lime-200 h-36 w-full md:w-1/2 pt-3 px-5'>
+        <img src='' />
+        <h3 className='font-bold text-lg text-gray-900'>{title}</h3>
+        <p className='text-sm text-gray-700 font-light'>{body}</p>
       </div>
+    )
+  }
+  return (
+    <main className='container max-w-screen-md mx-auto'>
+      <header className='w-full h-44 md:h-64 relative mb-5'>
+        <img className='w-full h-full object-cover object-center' src={urlImage} />
+        <div className='absolute bottom-0 left-1/2 transform -translate-x-1/2 w-full bg-gradient-to-t from-black text-center pb-5 pt-16'>
+          <h1 className='text-white text-2xl md:text-3xl font.bold'>Images around the world</h1>
+        </div>
+      </header>
+      <section className='flex flex-row justify-between items-center flex-wrap'>
+        {
+          posts.map(post => (
+            <Post key={post.id} title={post.title} body={post.body} />
+          ))
+        }
+      </section>
     </main>
   )
 }
